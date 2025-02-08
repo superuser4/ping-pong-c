204b0b2cb93aec43abbdd19a9cac2aab71d178a1
supercat7 <superuser3344@protonmail.com>
2025-02-08 16:56:36 -0500

    progress?

--- src/ping-pong.c
+++ src/ping-pong.c
@@ -3,6 +3,9 @@
 #include "math.h"
 #include "stdint.h"
 
+#include "ball.h"
+#include "player.h"
+
 // 2 libraries GLAD and GLFW for OpenGL
 #include "../include/glad/glad.h"  
 #include "/usr/include/GLFW/glfw3.h"
@@ -14,12 +17,13 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
-// function signs
+// function signatures
 int init();
 void processInput(GLFWwindow* window);
 void renderLoop(GLFWwindow* window);
 void drawBall();
 void updateProjection(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 // Function to initialize GLFW and GLAD
 int init() {
@@ -58,6 +62,12 @@ int init() {
     return 0;
 }
 
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 // Function to handle window resizing and updating the projection
 void updateProjection(GLFWwindow* window) {
     int width, height;
@@ -71,6 +81,7 @@ void updateProjection(GLFWwindow* window) {
     float aspectRatio = (float)width / (float)height;
     glOrtho(-1.0f * aspectRatio, 1.0f * aspectRatio, -1.0f, 1.0f, -1.0f, 1.0f); // Orthographic projection
     glMatrixMode(GL_MODELVIEW);     // Switch back to modelview matrix
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
 }
 
 void processInput(GLFWwindow* window) {
@@ -94,30 +105,6 @@ void renderLoop(GLFWwindow* window) {
     }
 }
 
-void drawBall() {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
-
-        // Set the circle parameters
-        float centerX = 0.0f;
-        float centerY = 0.0f;
-        float radius = 0.025f;
-        uint numSegments = 100;  // The number of triangle segments
-
-
-        glBegin(GL_TRIANGLE_FAN);
-        glColor4f(1.0f, 1.0f, 1.0f, 0.0f);
-        glVertex2f(centerX,centerY);
-
-        for (uint i=0; i <= numSegments; i++) {
-            float angle = i * (2.0f * M_PI / numSegments); // Calculate the angle for each segment
-            float x = centerX + radius * cos(angle);
-            float y = centerY + radius * sin(angle);
-            glVertex2f(x, y);  // Draw the point
-        }
-        
-        glEnd();
-        glFlush();
-}
 
 int main() {
     if (init() != 0) {
